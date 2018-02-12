02b140e8ac34d27ed13b8a190527a40b18a2f25b
Jeirsen <Jeirsen@gmail.com>
2018-02-11 17:17:31 -0800

    Re worked application layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require jquery3
+//= require rails-ujs
 //= require twitter/bootstrap
 //= require popper
 //= require bootstrap
