416fb908b30d28675b8201596d2020735a40c2e6
Jeirsen <Jeirsen@gmail.com>
2018-02-14 20:03:34 -0800

    Added New Create Vendor Action

--- app/controllers/vendor_controller.rb
+++ app/controllers/vendor_controller.rb
@@ -3,6 +3,50 @@ class VendorController < ApplicationController
     def index
     	@vendors = Vendor.all
     end
-    
+
+    def new
+        @vendor = Vendor.new
+    end
+
+    def edit
+        
+    end
+
+    def create
+      #render plain: params[:article].inspect
+      @vendor = Vendor.new(vendor_params)
+      if @vendor.save
+        flash[:success] = "Vendor was successfully created!!"
+        redirect_to list_vendors_path
+      else
+        render 'new'
+      end
+    end
+
+    def update
+        #if @article.update(article_params)
+            #flash[:success] = "Article was successfully updated!!"
+            #redirect_to article_path(@article)
+        #else
+            #render 'edit'
+        #end
+    end
+
+    def show
+        @vendor = Vendor.find(params[:id])
+    end
+
+    def destroy
+        #@article.destroy
+        #flash[:danger] = "Article was successfully deleted"
+        #redirect_to articles_path
+    end
+
+    private
+
+    def vendor_params
+        params.require(:vendor).permit(:name, :state, :address1, :zipcode, :address2, :phone, :city, :fax)
+    end
+
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root 'home#index'
 
   #vendors
-  get 'vendors' => 'vendor#index', :as => 'list_vendors'
+  get '/vendors' => 'vendor#index', :as => 'list_vendors'
+  get '/vendor/new' => 'vendor#new', :as => 'new_vendor'
+  get '/vendor/(:id)' => 'vendor#show', :as => 'vendor_detail'
+  post '/vendor' => 'vendor#create'
 
 end
