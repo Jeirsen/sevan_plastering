f6ed608aed83f0bb6a9975895cfba46c77e9af65
Jeirsen <jeirsen@gmail.com>
2018-08-14 21:13:11 -0500

    Full CRUD category products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,12 @@
 class Product < ApplicationRecord
 	belongs_to :unit
 	Status = {:active => 1, :inactive => 0}
-	Categories = {:stucco => 1, :pone => 2}
+	Categories = {:stucco => 1, :foam => 2}
 
 	def self.categories
 		[
         {id: Categories[:stucco], name: "Stucco"},
-        {id: Categories[:pone], name: "Pone"}
+        {id: Categories[:foam], name: "Foam"}
 		]
 	end
 end
