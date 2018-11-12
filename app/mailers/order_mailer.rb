336a9510e0f8d2f314416d8cf399806cbb013110
Jeirsen <jeirsen@gmail.com>
2018-11-12 18:26:22 -0500

    Added order number to subject mail

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,7 +5,7 @@ class OrderMailer < ApplicationMailer
 		mails = @order.vendor.vendor_emails.where(:status => VendorEmail::Status[:active]).map { |e| e.email } rescue []
 		if mails.present?
 			puts "-> Sending emails to the following destinations: #{mails}"
-			mail(to: mails, subject: 'New purchase order')
+			mail(to: mails, subject: 'New purchase order #' + @order.order_number.to_s)
 		else
 			puts "-> Vendor had no emails"
 		end
