c9336d3f8eb78802df0912a85e638460e72b0cdd
Jeirsen <jeirsen@gmail.com>
2022-10-30 23:16:55 -0500

    fixing email error/ added new gmail password app

--- config/application.rb
+++ config/application.rb
@@ -21,12 +21,14 @@ module SevanplasteringPorders
 
 
     config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
     config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
       domain:               'gmail.com',
       user_name:            'svmstucco@gmail.com',
-      password:             'jorgemontoya2018',
+      password:             'giicayhxlnwopltx',
       authentication:       'plain',
       enable_starttls_auto: true
     }
